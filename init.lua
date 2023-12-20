dfc8d2fc729ad2f59250ab21faf3f38372f73069
Christian Morrell <cmorrell@unb.ca>
2023-12-20 12:38:06 -0400

    Import set.lua

--- init.lua
+++ init.lua
@@ -72,5 +72,8 @@ require 'lsp-setup'
 -- (completion)
 require 'cmp-setup'
 
+-- Set IDE settings
+require 'set'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
