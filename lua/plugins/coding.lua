a44eb05050ab569db07447d43b4a80d435424e61
Christian Morrell <cmorrell@unb.ca>
2024-01-09 15:49:16 -0400

    Tmux Navigation Plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -134,5 +134,22 @@ return {
     end,
   },
   'tpope/vim-surround',
-  'easymotion/vim-easymotion'
+  'easymotion/vim-easymotion',
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  }
 }
