b58ef58cb23399cb3b5121099f41cb760119c757
Christian Morrell <cmorrell@unb.ca>
2024-01-13 18:05:17 -0400

    Add Custom Vim Commands
    Created script to add custom vim commands. Added a command that will show the current changes to the buffer before saving.

--- init.lua
+++ init.lua
@@ -74,5 +74,8 @@ require 'lsp-setup'
 -- (completion)
 require 'cmp-setup'
 
+-- Import custom commands
+require 'commands'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
