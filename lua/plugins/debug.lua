0bfcf0cdf4ee9855c13f95763bf14d3fb3663cf3
Christian Morrell <cmorrell@unb.ca>
2024-04-11 14:49:35 -0300

    Add floating window debug keymap

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -76,6 +76,7 @@ return {
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<leader>e', dapui.eval, { desc = 'Debug: Open floating window.'})
     
     -- Setup dapui to open every time a debugging session starts
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
